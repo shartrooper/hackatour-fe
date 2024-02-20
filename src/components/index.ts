fa9fa5c011dd08affc31227c9244e37400ca5e19
CaraRota <canalsdigital@gmail.com>
2024-02-19 22:39:07 -0300

    test

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,10 @@
-import React, { ReactNode } from 'react';
-import ReactDOM from 'react-dom';
+import React, { ReactNode } from "react";
+import ReactDOM from "react-dom";
 
 interface ModalPortalProps {
-	children: ReactNode;
+    children: ReactNode;
 }
 
 export const ModalPortal: React.FC<ModalPortalProps> = ({ children }) => {
-	return ReactDOM.createPortal(children, document.body);
+    return ReactDOM.createPortal(children, document.body);
 };
