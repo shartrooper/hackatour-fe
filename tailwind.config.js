069b6e05537428127bd9a85129dd209260ca689f
CaraRota <canalsdigital@gmail.com>
2024-02-21 12:51:42 -0300

    Change color palette to extend so we can use all tailwind colors and do not override them

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,27 +9,6 @@ export default {
     ],
     prefix: "",
     theme: {
-        colors: {
-            primary: "#493657",
-            secondary: "#CE7DA5",
-            ashGray: "#AAC0AF",
-            champagnePink: "#EFD5C3",
-            darkGreen: "#3E442B",
-
-            neptune: {
-                50: "#f2f9f9",
-                100: "#dfeeee",
-                200: "#c3ddde",
-                300: "#99c6c7",
-                400: "#7eb2b5",
-                500: "#4c898e",
-                600: "#427178",
-                700: "#3a5e64",
-                800: "#364f54",
-                900: "#304449",
-                950: "#1d2b2f",
-            },
-        },
         container: {
             center: true,
             padding: "2rem",
@@ -38,6 +17,27 @@ export default {
             },
         },
         extend: {
+            colors: {
+                primary: "#493657",
+                secondary: "#CE7DA5",
+                ashGray: "#AAC0AF",
+                champagnePink: "#EFD5C3",
+                darkGreen: "#3E442B",
+
+                neptune: {
+                    50: "#f2f9f9",
+                    100: "#dfeeee",
+                    200: "#c3ddde",
+                    300: "#99c6c7",
+                    400: "#7eb2b5",
+                    500: "#4c898e",
+                    600: "#427178",
+                    700: "#3a5e64",
+                    800: "#364f54",
+                    900: "#304449",
+                    950: "#1d2b2f",
+                },
+            },
             keyframes: {
                 "accordion-down": {
                     from: { height: "0" },
