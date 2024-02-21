6e6f5ead0d51d2a0a1922e2e45302838e23858a2
CaraRota <canalsdigital@gmail.com>
2024-02-21 09:47:43 -0300

    Create headers routes and views. Add Footer to avoid touching App.tsx in the near future

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+// Componenets
 import Header from "./components/header/Header";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./views/Homepage";
+import Flights from "./views/Flights";
+import Hotels from "./views/Hotels";
+import Cars from "./views/Cars";
+import Buses from "./views/Buses";
+import Trains from "./views/Trains";
+import Activities from "./views/Activities";
+import Login from "./views/sessions/Login";
+
+// RRD
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
@@ -10,7 +20,17 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path='/' element={<Homepage />} />
+                    {/* Sessions */}
+                    <Route path='/login' element={<Login />} />
+                    {/* Services */}
+                    <Route path='/flights' element={<Flights />} />
+                    <Route path='/hotels' element={<Hotels />} />
+                    <Route path='/cars' element={<Cars />} />
+                    <Route path='/buses' element={<Buses />} />
+                    <Route path='/trains' element={<Trains />} />
+                    <Route path='/activities' element={<Activities />} />
                 </Routes>
+                <Footer />
             </Router>
         </div>
     );
