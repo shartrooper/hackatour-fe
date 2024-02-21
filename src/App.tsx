3156eeeceeb4559cd7e54dabb5064129178bf9de
CaraRota <canalsdigital@gmail.com>
2024-02-21 09:36:05 -0300

    Install RRD and add HomePage view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from "./components/header/Header";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./views/Homepage";
+
 function App() {
     return (
         <div className='bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen'>
-            <Header />
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Homepage />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
---
+++ src/views/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-4xl font-bold'>Homepage</h1>
+        </div>
+    );
+};
+
+export default Homepage;
