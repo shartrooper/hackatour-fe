cbfaf0ac5cad963b386d4a3953e74d4618fb9ee4
CaraRota <canalsdigital@gmail.com>
2024-02-21 10:42:33 -0300

    Add outlined and normal buttons components

---
+++ src/components/ui/OutlinedButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// RRD
+import { Link } from "react-router-dom";
+
+interface AppButtonProps {
+    url: string;
+    children: React.ReactNode;
+    disabled?: boolean;
+}
+
+const AppButton: React.FC<AppButtonProps> = ({ children, url, disabled }) => {
+    return (
+        <Link
+            to={disabled ? "#" : url}
+            className={
+                disabled
+                    ? "border border-neptune-700 text-neptune-700 rounded-lg py-3 px-6 opacity-50 cursor-not-allowed"
+                    : "border border-neptune-700 text-neptune-700 rounded-lg py-3 px-6 hover:bg-neptune-50 active:bg-neptune-100 transition duration-300 ease-in-out"
+            }>
+            {children}
+        </Link>
+    );
+};
+
+export default AppButton;
