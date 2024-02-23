9d9eab13cde0b7fdca3922876aeee4d966f88a30
Marko <michinaux.marko@gmail.com>
2024-02-23 16:51:05 -0400

    proper implementation of bg images

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,13 @@
 import AppButton from "../ui/button/AppButton";
 import AppInput from "../ui/input/AppInput";
-import heroImage from "/images/hero-image.jpg";
 import CarouselOffers from "@/components/hero/CarouselOffers";
 
 const Hero = () => {
-    return (
-        <div className='w-full h-full bg-cover' style={{ backgroundImage: `url(${heroImage})` }}>
-            <div className='flex flex-col justify-center items-center'>
-                <h1
-                    className='my-5
+  return (
+    <div className="w-full h-full bg-cover bg-hero-sm lg:bg-hero-lg">
+      <div className="flex flex-col justify-center items-center">
+        <h1
+          className="my-5
         bg-cover
         mx-auto
         w-11/12
@@ -25,26 +24,27 @@ const Hero = () => {
         lg:max-w-screen-sm
         shadow-neptune-500
         text-neptune-50
-        bg-neptune-400'>
-                    El placer de viajar juntos
-                </h1>
-                <div className='mb-5'>
-                    <CarouselOffers />
-                </div>
-            </div>
-            <div className='flex justify-center items-center mb-5 md:max-w-lg md:mx-auto'>
-                <div className='w-11/12 bg-slate-50 opacity-95 rounded-lg p-4 border border-neptune-400 shadow-xl'>
-                    <h2 className='text-center text-zinc-800 text-lg lg:text-2xl font-bold'>
-                        Encuentra tu próximo destino
-                    </h2>
-                    <div className='flex flex-col lg:flex-row lg:items-end gap-5 justify-center items-center mt-5'>
-                        <AppInput placeholder='Buscar' />
-                        <AppButton url='/search'>Buscar</AppButton>
-                    </div>
-                </div>
-            </div>
+        bg-neptune-400"
+        >
+          El placer de viajar juntos
+        </h1>
+        <div className="mb-5">
+          <CarouselOffers />
         </div>
-    );
+      </div>
+      <div className="flex justify-center items-center mb-5 md:max-w-lg md:mx-auto">
+        <div className="w-11/12 bg-slate-50 opacity-95 rounded-lg p-4 border border-neptune-400 shadow-xl">
+          <h2 className="text-center text-zinc-800 text-lg lg:text-2xl font-bold">
+            Encuentra tu próximo destino
+          </h2>
+          <div className="flex flex-col lg:flex-row lg:items-end gap-5 justify-center items-center mt-5">
+            <AppInput placeholder="Buscar" />
+            <AppButton url="/search">Buscar</AppButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,62 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-        "./pages/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./app/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-    ],
-    prefix: "",
-    theme: {
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
-        },
-        extend: {
-            colors: {
-                primary: "#493657",
-                secondary: "#CE7DA5",
-                ashGray: "#AAC0AF",
-                champagnePink: "#EFD5C3",
-                darkGreen: "#3E442B",
-
-                neptune: {
-                    50: "#f2f9f9",
-                    100: "#dfeeee",
-                    200: "#c3ddde",
-                    300: "#99c6c7",
-                    400: "#7eb2b5",
-                    500: "#4c898e",
-                    600: "#427178",
-                    700: "#3a5e64",
-                    800: "#364f54",
-                    900: "#304449",
-                    950: "#1d2b2f",
-                },
-            },
-            keyframes: {
-                "accordion-down": {
-                    from: { height: "0" },
-                    to: { height: "var(--radix-accordion-content-height)" },
-                },
-                "accordion-up": {
-                    from: { height: "var(--radix-accordion-content-height)" },
-                    to: { height: "0" },
-                },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-            },
-        },
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    plugins: ["tailwindcss-animate"],
+    extend: {
+      colors: {
+        primary: "#493657",
+        secondary: "#CE7DA5",
+        ashGray: "#AAC0AF",
+        champagnePink: "#EFD5C3",
+        darkGreen: "#3E442B",
+
+        neptune: {
+          50: "#f2f9f9",
+          100: "#dfeeee",
+          200: "#c3ddde",
+          300: "#99c6c7",
+          400: "#7eb2b5",
+          500: "#4c898e",
+          600: "#427178",
+          700: "#3a5e64",
+          800: "#364f54",
+          900: "#304449",
+          950: "#1d2b2f",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+    backgroundImage: {
+      "hero-sm": "url('/images/hero-image-sm.jpg')",
+      "hero-lg": "url('/images/hero-image-lg.jpg')",
+    },
+  },
+  plugins: ["tailwindcss-animate"],
 };
