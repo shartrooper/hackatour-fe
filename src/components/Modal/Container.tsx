9d7a4229248974519e0e0d30cd245a597b628f32
Marko <michinaux.marko@gmail.com>
2024-02-23 12:46:16 -0400

    add container ref

--- src/components/Modal/Container.tsx
+++ src/components/Modal/Container.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { ModalPortal } from "./Wrapper";
 
 interface ProductModalProps {
@@ -6,10 +6,29 @@ interface ProductModalProps {
 }
 
 export const ModalContainer: React.FC<ProductModalProps> = ({ onClose }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClick(event: MouseEvent) {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, [onClose]);
+
   return (
     <ModalPortal>
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75">
-        <div className="bg-white p-8 max-w-md">
+        <div ref={containerRef} className="bg-white p-8 max-w-md">
           <div className="flex justify-end mb-2">
             <button onClick={onClose}>Close</button>
           </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,63 +9,72 @@ import HeaderIcons from "../ui/IconWrapper";
 
 // RRD
 import { Link } from "react-router-dom";
+import { ModalContainer } from "../Modal/Container";
+import { useState } from "react";
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggle = () => setShowModal((prev) => !prev);
+
   return (
-    <div className="bg-neptune-400 text-neptune-900 shadow-lg py-2 lg:flex lg:items-center">
-      <div className="text-neptune-900 font-bold text-center uppercase text-lg lg:text-xl lg:ml-5">
-        <Link to="/">HackaTour</Link>
-        <div className="lg:hidden inline ml-5 text-neptune-900">
-          <button className="cursor-pointer">
+    <>
+      <div className="bg-neptune-400 text-neptune-900 shadow-lg py-2 lg:flex lg:items-center">
+        <div className="text-neptune-900 font-bold text-center uppercase text-lg lg:text-xl lg:ml-5">
+          <Link to="/">HackaTour</Link>
+          <div className="lg:hidden inline ml-5 text-neptune-900">
+            <button onClick={handleToggle} className="cursor-pointer">
+              <AccountCircleOutlinedIcon />
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center flex-wrap mt-2 gap-5 lg:mt-0 lg:mx-auto">
+          {/* Avion */}
+          <HeaderIcons
+            icon={<AirplaneTicketOutlinedIcon />}
+            title="Vuelos"
+            link="/flights"
+          />
+          {/* Hotel */}
+          <HeaderIcons
+            icon={<BedOutlinedIcon />}
+            title="Hoteles"
+            link="/hotels"
+          />
+          {/* Auto */}
+          <HeaderIcons
+            icon={<TimeToLeaveOutlinedIcon />}
+            title="Autos"
+            link="/cars"
+          />
+          {/* Colectivo */}
+          <HeaderIcons
+            icon={<DirectionsBusFilledOutlinedIcon />}
+            title="Micros"
+            link="/buses"
+          />
+          {/* Tren */}
+          <HeaderIcons
+            icon={<TrainOutlinedIcon />}
+            title="Trenes"
+            link="/trains"
+          />
+          {/* Excursiones */}
+          <HeaderIcons
+            icon={<KayakingOutlinedIcon />}
+            title="Salidas"
+            link="/activities"
+          />
+        </div>
+        <div className="hidden lg:flex lg:justify-center lg:mr-5 text-neptune-900">
+          <Link to="/login" className="cursor-pointer">
             <AccountCircleOutlinedIcon />
-          </button>
+            <div className="hidden lg:inline lg:pl-2">Iniciar Sesion</div>
+          </Link>
         </div>
       </div>
-      <div className="flex justify-center flex-wrap mt-2 gap-5 lg:mt-0 lg:mx-auto">
-        {/* Avion */}
-        <HeaderIcons
-          icon={<AirplaneTicketOutlinedIcon />}
-          title="Vuelos"
-          link="/flights"
-        />
-        {/* Hotel */}
-        <HeaderIcons
-          icon={<BedOutlinedIcon />}
-          title="Hoteles"
-          link="/hotels"
-        />
-        {/* Auto */}
-        <HeaderIcons
-          icon={<TimeToLeaveOutlinedIcon />}
-          title="Autos"
-          link="/cars"
-        />
-        {/* Colectivo */}
-        <HeaderIcons
-          icon={<DirectionsBusFilledOutlinedIcon />}
-          title="Micros"
-          link="/buses"
-        />
-        {/* Tren */}
-        <HeaderIcons
-          icon={<TrainOutlinedIcon />}
-          title="Trenes"
-          link="/trains"
-        />
-        {/* Excursiones */}
-        <HeaderIcons
-          icon={<KayakingOutlinedIcon />}
-          title="Salidas"
-          link="/activities"
-        />
-      </div>
-      <div className="hidden lg:flex lg:justify-center lg:mr-5 text-neptune-900">
-        <Link to="/login" className="cursor-pointer">
-          <AccountCircleOutlinedIcon />
-          <div className="hidden lg:inline lg:pl-2">Iniciar Sesion</div>
-        </Link>
-      </div>
-    </div>
+      {showModal && <ModalContainer onClose={handleToggle} />}
+    </>
   );
 };
 
